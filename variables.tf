06d4d0ac6028cf04ffd6afcd3f8f0ce160782a35
Alex, Lee <cine0831@gmail.com>
2020-07-11 15:49:32 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,12 +2,12 @@ variable "region" {
   description = "AWS Region"
   type        = string
   #default     = "ap-northeast-2"
-  default     = ""
+  #default     = ""
 }
 
 variable "s3-bucket" {
   description = "AWS S3 Bucket - default awx"
   type        = string
   #default     = "s3-bucket-awx"
-  default     = ""
+  #default     = ""
 }
