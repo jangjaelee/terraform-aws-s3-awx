f02f1caa850cf47ab878c2ce2a631c1be6a38ff8
Alex, Lee <cine0831@gmail.com>
2020-07-12 00:31:53 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,9 +4,29 @@
 variable "region" {
   description = "AWS Region"
   type        = string
+  #default     = "ap-northeast-2"
 }
 
 variable "s3-bucket" {
   description = "AWS S3 Bucket - default awx"
   type        = string
+  #default     = "s3-awx-bucket-newtest"
+}
+
+variable "public_domain_name" {
+  description = "public shop domain_name"
+  type        = string
+  #default     = "awx.co.kr"
+}
+
+variable "private_domain_name" {
+  description = "private shop domain_name"
+  type        = string
+  #default     = "awx.internal"
+}
+
+variable "vpc-tagname" {
+  description = "tag:Name of VPC-awx"
+  type        = string
+  #default     = "VPC-awx"
 }
