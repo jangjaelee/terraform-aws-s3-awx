69d4aff4a7d8cdc59fbf916fb9422d11f8166d79
Alex, Lee <cine0831@gmail.com>
2020-07-11 15:36:55 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "s3_bucket" {
-  source = "github.com/jangjaelee/module-terraform-aws-s3.git"
-
+  #source = "github.com/jangjaelee/module-terraform-aws-s3.git"
+  source  = "app.terraform.io/cine0831/vpc/aws"
+  version = "0.1.0"
+  
   region = var.region
   s3-bucket = var.s3-bucket
 }
