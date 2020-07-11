343de98dbab7e164d52cf1216c0e29d61e2ab1ea
Alex, Lee <cine0831@gmail.com>
2020-07-12 00:34:09 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ module "s3_bucket" {
 module "route53" {
   #source = "github.com/jangjaelee/module-terraform-aws-route53.git"
   source  = "app.terraform.io/cine0831/route53/aws"
-
+  version = "0.1.0"
+  
   vpc-tagname = var.vpc-tagname
   public_domain_name  = var.public_domain_name
   private_domain_name = var.private_domain_name
