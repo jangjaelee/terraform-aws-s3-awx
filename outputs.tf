5ed40c7a19b9506df3608a95c6f1808974970c32
Alex, Lee <cine0831@gmail.com>
2020-07-12 00:31:39 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,11 @@ output "s3_bucket_name" {
 output "s3_bucket_origin_region" {
   value = module.s3_bucket.s3_bucket_origin_region
 }
+
+output "route53-public" {
+  value = module.route53.route53-public
+}
+
+output "route53-private" {
+  value = module.route53.route53-private
+}
