5be4399b69af80ef1b607bd7fbb88615b5e68e6a
Alex, Lee <cine0831@gmail.com>
2020-07-11 18:28:11 +0900

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,6 @@
-#terraform {
-#  required_version = ">= 0.12.26"
-
-#  required_providers {
-#    aws        = ">= 2.65.0"
-#  }
-#}
+terraform {
+  required_version = ">= 0.12.28"
+  required_providers {
+    aws        = ">= 2.65.0"
+  }
+}
