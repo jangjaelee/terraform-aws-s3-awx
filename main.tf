507726ce3581bc602c45e019c29638a94b797892
Alex, Lee <cine0831@gmail.com>
2020-07-12 00:40:21 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 //--------------------------------------------------------------------
 // Modules
 
-region = var.region
-
 module "s3_bucket" {
   #source = "github.com/jangjaelee/module-terraform-aws-s3.git"
   source  = "app.terraform.io/cine0831/s3/aws"
   version = "0.1.0"
-  
+
+  region = var.region
   s3-bucket = var.s3-bucket
 }
 
@@ -16,6 +15,7 @@ module "route53" {
   source  = "app.terraform.io/cine0831/route53/aws"
   version = "0.1.0"
   
+  region = var.region
   vpc-tagname = var.vpc-tagname
   public_domain_name  = var.public_domain_name
   private_domain_name = var.private_domain_name
